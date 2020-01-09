366eee2199989df9f411bac5ede1e8edb172668e
s18b715 <s18b715@shohoku.ac.jp>
2020-01-09 13:35:26 +0900

    ホームの曜日ボタンサイズの”高さ”の調整モデル作成

--- app/src/main/java/jp/ac/shohoku/umairecipe/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/umairecipe/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,7 @@ import android.widget.TextView;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.main_home);
 
-        //今日の曜日の色だけ変える
-        Calendar calendar = Calendar.getInstance();
-        int week = calendar.get(Calendar.DAY_OF_WEEK);
-        int[] color = {255, 160, 160};
-        if (week == 1) {
-            TextView SunText1 = (TextView) findViewById(R.id.SunText1);
-            SunText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
-        } else if (week == 2) {
-            TextView MonText1 = (TextView) findViewById(R.id.MonText1);
-            MonText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
-        } else if (week == 3) {
-            TextView TueText1 = (TextView) findViewById(R.id.TueText1);
-            TueText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
-        } else if (week == 4) {
-            TextView WedText1 = (TextView) findViewById(R.id.WedText1);
-            WedText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
-        } else if (week == 5) {
-            TextView ThuText1 = (TextView) findViewById(R.id.ThuText1);
-            ThuText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
-        } else if (week == 6) {
-            TextView FriText1 = (TextView) findViewById(R.id.FriText1);
-            FriText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
-        } else if (week == 7) {
-            TextView SatText1 = (TextView) findViewById(R.id.SatText1);
-            SatText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
-        };
+        //今日の曜日の色を変える
+        WeekColor();
+        //ボタンなどのサイズを画面サイズに合わせて変更する
+        setSize();
 
         //ボタンを押したときにイベントを取得できるようにする
         //メニューボタン
@@ -149,5 +130,80 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    public void WeekColor(){
+        //今日の曜日の色だけ変える
+        Calendar calendar = Calendar.getInstance();
+        int week = calendar.get(Calendar.DAY_OF_WEEK);
+        int[] color = {255, 160, 160};
+        if (week == 1) {
+            TextView SunText1 = (TextView) findViewById(R.id.SunText1);
+            SunText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        } else if (week == 2) {
+            TextView MonText1 = (TextView) findViewById(R.id.MonText1);
+            MonText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        } else if (week == 3) {
+            TextView TueText1 = (TextView) findViewById(R.id.TueText1);
+            TueText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        } else if (week == 4) {
+            TextView WedText1 = (TextView) findViewById(R.id.WedText1);
+            WedText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        } else if (week == 5) {
+            TextView ThuText1 = (TextView) findViewById(R.id.ThuText1);
+            ThuText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        } else if (week == 6) {
+            TextView FriText1 = (TextView) findViewById(R.id.FriText1);
+            FriText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        } else if (week == 7) {
+            TextView SatText1 = (TextView) findViewById(R.id.SatText1);
+            SatText1.setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+        }
+    }
+
+
+    public void setSize(){
+
+        // 画面サイズを取得する
+        Display display = getWindowManager().getDefaultDisplay();
+        Point p = new Point();
+        display.getSize(p);
+        int width = p.x;
+        int height = p.y;
+
+        TextView[] textView1s = {(TextView) findViewById(R.id.SunText1),
+                (TextView) findViewById(R.id.MonText1),
+                (TextView) findViewById(R.id.TueText1),
+                (TextView) findViewById(R.id.WedText1),
+                (TextView) findViewById(R.id.ThuText1),
+                (TextView) findViewById(R.id.FriText1),
+                (TextView) findViewById(R.id.SatText1)};
+
+        TextView[] textView2s = {(TextView) findViewById(R.id.SunText2),
+                (TextView) findViewById(R.id.MonText2),
+                (TextView) findViewById(R.id.TueText2),
+                (TextView) findViewById(R.id.WedText2),
+                (TextView) findViewById(R.id.ThuText2),
+                (TextView) findViewById(R.id.FriText2),
+                (TextView) findViewById(R.id.SatText2)};
+
+        Button[] buttons = {(Button) findViewById(R.id.SunButton),
+                (Button) findViewById(R.id.MonButton),
+                (Button) findViewById(R.id.TueButton),
+                (Button) findViewById(R.id.WedButton),
+                (Button) findViewById(R.id.ThuButton),
+                (Button) findViewById(R.id.WedButton),
+                (Button) findViewById(R.id.SatButton)};
+
+        int Hsize = height / 10;
+        int Wsize = width -100;
+        for (int i = 0; i<7; i++){
+            textView1s[i].setHeight(Hsize);
+
+            textView2s[i].setHeight(Hsize);
+            buttons[i].setHeight(Hsize);
+        }
+
     }
 }
