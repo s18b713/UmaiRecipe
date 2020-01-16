51c89b8fe67c4551bf4edd9bb7fecfcbccec3ba8
s18b715 <s18b715@shohoku.ac.jp>
2020-01-16 16:38:37 +0900

    いろいろやりました。

--- app/src/main/java/jp/ac/shohoku/umairecipe/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/umairecipe/MainActivity.java
@@ -1,24 +1,38 @@
 package jp.ac.shohoku.umairecipe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.AppBarConfiguration;
+//import androidx.navigation.ui.NavigationUI;
+
+//import com.google.android.material.navigation.NavigationView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Calendar;
 
+//import android.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -130,6 +144,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        setContentView(R.layout.main_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_add, R.id.nav_like)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 
 
---
+++ app/src/main/java/jp/ac/shohoku/umairecipe/ui/add/AddViewModel.java
@@ -0,0 +1,20 @@
+package jp.ac.shohoku.umairecipe.ui.add;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+
+public class AddViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AddViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is add fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
